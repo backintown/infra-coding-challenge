50cf18e7d4b6b22b124b1652a2a09de4fec27913
Alex Cheng <cheng.alex5@gmail.com>
2018-09-04 16:26:23 -0500

    changed index settings

--- nodejs/solution.js
+++ nodejs/solution.js
@@ -5,7 +5,7 @@ const { createRecord, setupElasticsearch } = require('./utils');
 
 (async () => {
     await setupElasticsearch();
-    await seedRedis(10000);
+    await seedRedis(12000);
     await importRecords();
 })();
 
@@ -14,37 +14,46 @@ async function importRecords() {
 
     const redis = new Redis({ host: 'redis' });
     let record;
-    let records = [];
-    let chunks = [];
-
-    for (let i = 0; i < 100; i++) {
-        chunks[i] = [];
-    }
+    let chunk = [];
+    let j = 0;
     do {
         record = await redis.rpop('records');
-        record && records.push(record);
-
-    } while (record);
-    console.log('splitting...');
-
-    let k = 0;
-    //assemble bulk body
-    for (let i = 0; i < 100; i++) {
-        for (let j = 0; j < Math.floor(records.length / 100); j++) {
-            chunks[i].push({
+        // record && records.push(record);
+        if (record) {
+            chunk.push({
                 index: {
                     _index: 'records',
                     _type: 'all'
                 }
-            });
-            chunks[i].push(records[k]);
-            k++
+            })
+            chunk.push(record);
         }
-    }
+        j++;
+        if (j % 1000 === 0) {
+            console.log('importing');
+            await elasticsearch.bulk({ body: chunk }).then(_ => {
+                chunk = [];
+            });
+        }
+    } while (record);
 
-    for (let i = 0; i < chunks.length; i++) {
-        await elasticsearch.bulk({ body: chunks[i] });
-    }
+    //assemble bulk body
+    // let i = j = 0;
+    // while (j < records.length) {
+    //     chunks[i].push({
+    //         index: {
+    //             _index: 'records',
+    //             _type: 'all'
+    //         }
+    //     });
+    //     chunks[i].push(records[j]);
+    //     j++;
+    //     if (j % 100 === 0) i++;
+    // }
+
+    // for (let i = 0; i < chunks.length; i++) {
+    //     await elasticsearch.bulk({ body: chunks[i] });
+    // }
 
     await redis.disconnect();
 }
